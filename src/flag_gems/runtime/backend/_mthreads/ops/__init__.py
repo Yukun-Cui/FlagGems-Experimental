c8ca5c5a3a302783c29267fd4b97166ceb3a453c
1572161937 <1572161937@qq.com>
2026-09-08 10:49:25 +0800

    [KernelGen][MThreads] Add linalg_ldl_solve Moore Threads specialized operator

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -49,6 +49,7 @@ from .index_copy_ import index_copy, index_copy_
 from .index_put import _index_put_impl_, index_put, index_put_
 from .index_select import index_select
 from .linalg_cholesky import linalg_cholesky
+from .linalg_ldl_solve import linalg_ldl_solve
 from .linear import linear
 from .log import log
 from .log10 import log10, log10_, log10_out
@@ -143,6 +144,7 @@ __all__ = [
     "_index_put_impl_",
     "index_select",
     "linalg_cholesky",
+    "linalg_ldl_solve",
     "linear",
     "log",
     "log10",
