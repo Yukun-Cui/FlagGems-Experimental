5bb554d98b4dc95216e909a54bb4c8f21e01bfd8
1572161937 <1572161937@qq.com>
2026-09-09 20:45:19 +0800

    [KernelGen][MThreads] Add addmv_ Moore Threads specialized operator

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -19,6 +19,7 @@ from ._thnn_fused_lstm_cell_backward_impl import _thnn_fused_lstm_cell_backward_
 from ._upsample_bilinear2d_aa import _upsample_bilinear2d_aa
 from ._upsample_nearest_exact2d_backward import _upsample_nearest_exact2d_backward
 from .adaptive_avg_pool2d_backward import _adaptive_avg_pool2d_backward
+from .addmv_ import addmv_
 from .all import all, all_dim, all_dims
 from .amax import amax
 from .any import any, any_dim, any_dims
@@ -127,6 +128,7 @@ __all__ = [
     "_thnn_fused_lstm_cell_backward_impl",
     "_upsample_bilinear2d_aa",
     "_upsample_nearest_exact2d_backward",
+    "addmv_",
     "amax",
     "all",
     "all_dim",
